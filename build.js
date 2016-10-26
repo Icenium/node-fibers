4867ea8351dce9a38090a291fcd87fdeff331fbf
rosen-vladimirov <rosen.vladimirov@telerik.com>
2016-10-26 11:02:33 +0300

    Revert "Use `process.versions.modules` to select binary"
    This reverts commit 09863ddcb93889fb3c6b0b0d555c114d26172ba8.

--- build.js
+++ build.js
@@ -7,7 +7,8 @@ var cp = require('child_process'),
 var force = false, debug = false;
 var
 	arch = process.arch,
-	platform = process.platform;
+	platform = process.platform,
+	v8 = /[0-9]+\.[0-9]+/.exec(process.versions.v8)[0];
 var args = process.argv.slice(2).filter(function(arg) {
 	if (arg === '-f') {
 		force = true;
@@ -28,7 +29,7 @@ if (!{ia32: true, x64: true, arm: true, arm64: true, ppc: true, ppc64: true, s39
 }
 
 // Test for pre-built library
-var modPath = platform+ '-'+ arch+ '-'+ process.versions.modules;
+var modPath = platform+ '-'+ arch+ '-v8-'+ v8;
 if (!force) {
 	try {
 		fs.statSync(path.join(__dirname, 'bin', modPath, 'fibers.node'));
@@ -100,9 +101,4 @@ function afterBuild() {
 	}
 	fs.renameSync(targetPath, installPath);
 	console.log('Installed in `'+ installPath+ '`');
-	if (process.versions.electron) {
-		process.nextTick(function() {
-			require('electron').app.quit();
-		});
-	}
 }
--- fibers.js
+++ fibers.js
@@ -7,17 +7,13 @@ var fs = require('fs'), path = require('path');
 Math.random();
 
 // Look for binary for this platform
-var modPath = path.join(__dirname, 'bin', process.platform+ '-'+ process.arch+ '-'+ process.versions.modules, 'fibers');
+var v8 = 'v8-'+ /[0-9]+\.[0-9]+/.exec(process.versions.v8)[0];
+var modPath = path.join(__dirname, 'bin', process.platform+ '-'+ process.arch+ '-'+ v8, 'fibers');
 try {
 	fs.statSync(modPath+ '.node');
 } catch (ex) {
 	// No binary!
-	console.error(
-		'## There is an issue with `node-fibers` ##\n'+
-		'`'+ modPath+ '.node` is missing.\n\n'+
-		'Try running this to fix the issue: '+ process.execPath+ ' '+ __dirname.replace(' ', '\\ ')+ '/build'
-	);
-	throw new Error('Missing binary. See message above.');
+	throw new Error('`'+ modPath+ '.node` is missing. Try reinstalling `node-fibers`?');
 }
 
 // Pull in fibers implementation
